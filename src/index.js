fa0bd6d1c3b704e85c66bab81ae5bebb677c4349
Lucas Garron <code@garron.net>
2021-04-15 01:09:33 -0700

    Split out most stats code into a separate file.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
-import { windowGlobals, initialize } from "./qqtimer";
+import { qqtimerWindowGlobals, initialize } from "./qqtimer";
+import { statsWindowGlobals } from "./stats";
 
-for (const [name, fn] of Object.entries(windowGlobals)) {
+for (const [name, fn] of Object.entries(qqtimerWindowGlobals)) {
+  globalThis[name] = fn;
+}
+
+for (const [name, fn] of Object.entries(statsWindowGlobals)) {
   globalThis[name] = fn;
 }
 
